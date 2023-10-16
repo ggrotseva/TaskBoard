fa353f8689cafa0fb4419122f8ee9f9e8f5c7acd
ggrotseva <g.grotseva@gmail.com>
2023-10-16 14:24:07 +0300

    configured terraform

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.75.0"
     }
   }
@@ -12,8 +12,8 @@ provider "azurerm" {
 }
 
 resource "random_integer" "ri" {
-    min = 10000
-    max = 99999
+  min = 10000
+  max = 99999
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -29,8 +29,33 @@ resource "azurerm_service_plan" "plan" {
   sku_name            = "F1"
 }
 
+resource "azurerm_mssql_server" "server" {
+  name                         = "mssqlserver${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "adminGabs"
+  administrator_login_password = "Password12#"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "taskboard-db-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  sku_name       = "Basic"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "dbrule" {
+  name             = "FirewallRuleDb"
+  server_id        = azurerm_mssql_server.server.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
 resource "azurerm_linux_web_app" "app" {
-  name                = "taskboard"
+  name                = "taskboard${random_integer.ri.result}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.plan.location
   service_plan_id     = azurerm_service_plan.plan.id
@@ -41,29 +66,17 @@ resource "azurerm_linux_web_app" "app" {
     }
     always_on = false
   }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.server.administrator_login};Password=${azurerm_mssql_server.server.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
 }
 
 resource "azurerm_app_service_source_control" "git" {
-  app_id   = azurerm_linux_web_app.app.id
-  repo_url = "https://github.com/ggrotseva/TaskBoard"
-  branch   = "master"
+  app_id                 = azurerm_linux_web_app.app.id
+  repo_url               = "https://github.com/ggrotseva/TaskBoard"
+  branch                 = "master"
   use_manual_integration = true
-}
-
-resource "azurerm_mssql_server" "mssql" {
-  name                         = "mssqlserver"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = "admin"
-  administrator_login_password = "Password12#"
-
-  azuread_administrator {
-    login_username = "AzureAD Admin"
-    object_id      = "00000000-0000-0000-0000-000000000000"
-  }
-
-  tags = {
-    environment = "production"
-  }
 }
